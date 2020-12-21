76c69d9b7428edad78132334f77da7a4d883af09
shruthi64 <65290535+shruthi64@users.noreply.github.com>
2020-12-21 08:35:00 +0530

    Added files

---
+++ 07-Dictionary-TCP/clientDict_TCP.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<arpa/inet.h>
+# define port 5000
+void main()
+{
+struct sockaddr_in serveraddr;
+int clisocket;
+char buffer[100];
+clisocket=socket(PF_INET,SOCK_STREAM,0);
+if(clisocket>0)
+printf("\nclient socket created");
+serveraddr.sin_family= PF_INET;
+serveraddr.sin_port= htons(port);
+serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+connect(clisocket,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+printf("\nEnter word to be searched in dictionary : ");
+scanf("%s",buffer);
+send(clisocket,buffer,sizeof(buffer),0);
+recv(clisocket,buffer,sizeof(buffer),0);
+printf("Meaning - %s\n",buffer);
+recv(clisocket,buffer,sizeof(buffer),0);
+printf("Antonym - %s\n",buffer);
+close(clisocket);
+}
